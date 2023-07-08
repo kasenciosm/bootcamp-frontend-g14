71aef986f528a6840cfdc85b9e1520122e4b85d8
Kenyi Asencios <asenciosm.k@gmail.com>
2023-07-07 19:14:11 -0500

    ultimos cambios

---
+++ Semana05/dia02-aprendiendojs/app.js
@@ -0,0 +1,34 @@
+console.log('Hola chicos soy el javascript')
+
+
+// Tipos de datos en Javascript
+//Numbers ==> operadores aritméticos (+ * /)
+//módulo ==> residuo
+console.log(10%3);
+
+//String = operador unitario
+console.log(typeof("Hola mi nombre es Kenyi"));
+
+//Typeof()
+
+//Booleans ==> operadores comparativos (>, <, = )
+
+//true(verdadero)
+//false(falso)
+
+console.log(typeof(3 < 5));
+console.log(10<15);
+
+//CONVERSION AUTOMATICA ==> COERCIÓN DE TIPO
+
+console.log('5'-1);
+console.log(typeof('5'+1));
+
+//VARIABLES
+
+let age = 18;
+let nombre;
+nombre = 'Kenyi'
+console.log(nombre);
+
+const namePersona = 'Kenyi';
